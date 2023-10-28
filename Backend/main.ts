814687cb36fd7bd6b66135771d130bbe9dd88da3
JustinGebertDesk <justin.gebert.berlin@gmail.com>
2023-10-28 21:51:51 +0200

    npm set up

---
+++ Backend/main.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
