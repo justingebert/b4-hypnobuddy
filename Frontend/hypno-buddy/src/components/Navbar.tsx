e98b02231a86f97aa7c6db77f0c878ceab8e8924
snahn2209 <106750285+snahn2209@users.noreply.github.com>
2023-11-18 16:52:34 +0100

    fixed LogoutButton

--- Frontend/hypno-buddy/src/components/Navbar.tsx
+++ Frontend/hypno-buddy/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import LogoutButton from './LogoutButton';
 import Logo from '../assets/hb.svg';
 import styles from '../styles/Navbar.module.css';
@@ -6,6 +6,12 @@ import {useAuth} from "../contexts/AuthContext.tsx";
 
 const Navbar = () => {
     const { isAuthenticated, user, handleLogout } = useAuth();
+    const navigate = useNavigate();
+    const handleLogoutClick = async () => {
+        await handleLogout();
+        navigate("/");
+        // You may also add a redirect here if needed
+    };
 
     return (
         <nav className={styles.navbar}>
@@ -18,7 +24,7 @@ const Navbar = () => {
                 {isAuthenticated && user ? (
                  //TODO i think is is not a good approach to let 2 fetches go before rendering
                     <>
-                        <LogoutButton onLogout={handleLogout} className={styles.logoutButton} />
+                        <LogoutButton onLogout={handleLogoutClick} className={styles.logoutButton} />
                         <div className={styles.userEmail}>{user.name.first + " " + user.name.last}</div>
                     </>
                 ) : (
