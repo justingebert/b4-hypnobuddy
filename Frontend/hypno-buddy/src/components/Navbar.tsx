b5be17cb0211e928282a1c49dfcbdb416954f0ff
maile000 <148875772+maile000@users.noreply.github.com>
2023-11-19 12:15:15 +0100

    Bootstrap style (#79)
    * - bootstrap install
    
    * - update app.css navbar

--- Frontend/hypno-buddy/src/components/Navbar.tsx
+++ Frontend/hypno-buddy/src/components/Navbar.tsx
@@ -5,24 +5,79 @@ import styles from '../styles/Navbar.module.css';
 
 const Navbar = ({ isLoggedIn, handleLogout }:any) => {
     return (
-        <nav className={styles.navbar}>
-            <Link to="/" className={styles.logoLink}>
-                <img src={Logo} alt="logo" className={styles.logo} />
-                <h1 className={styles.title}>Hypno Buddy</h1>
-            </Link>
 
-            <div className={styles.navItems}>
-                {isLoggedIn.isAuthenticated && isLoggedIn.user ? ( //TODO i think is is not a good approach to let 2 fetches go before rendering
-                    <>
-                        <LogoutButton onLogout={handleLogout} className={styles.logoutButton} />
-                        <div className={styles.userEmail}>{isLoggedIn.user.name.first + " " + isLoggedIn.user.name.last}</div>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login" className={styles.navLink}>Login</Link>
-                        <Link to="/register" className={styles.navLink}>Register</Link>
-                    </>
-                )}
+        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+            <div className="container-fluid">
+                <Link to="/" className={styles.logoLink}>
+                    <img src={Logo} alt="logo" className={styles.logo} />
+                    <h1 className={styles.title}>Hypno Buddy</h1>
+                </Link>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link active" aria-current="page">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/features" className="nav-link">
+                                Features
+                            </Link>
+                        </li>
+
+                        <li className="nav-item dropdown">
+                            {isLoggedIn.isAuthenticated && isLoggedIn.user ? (
+                                <>
+                                    <LogoutButton onLogout={handleLogout} className={styles.logoutButton} />
+                                    <div className={styles.userEmail}>
+                                        {isLoggedIn.user.name.first} {isLoggedIn.user.name.last}
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <Link to="/login" className={styles.navLink}>
+                                        Login
+                                    </Link>
+                                    <Link to="/register" className={styles.navLink}>
+                                        Register
+                                    </Link>
+                                </>
+                            )}
+
+                            {/* Dropdown-Elemente */}
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <Link to="/action" className="dropdown-item">
+                                        Action
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/another-action" className="dropdown-item">
+                                        Another Action
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/something-else" className="dropdown-item">
+                                        Something Else Here
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
