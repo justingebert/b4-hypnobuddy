7821fa2fb77685e95430ff4715f8dfecdd7e158e
snahn2209 <106750285+snahn2209@users.noreply.github.com>
2023-11-30 17:05:43 +0100

    implemented patient dosanddonts page, routes dont work yet

---
+++ Frontend/hypno-buddy/src/pages/DosAndDontsPatientPage.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from "react";
+import {useAuth} from "../contexts/AuthContext.tsx";
+
+const DosAndDontsPatientPage = ({ fearId }) => {
+    const {isAuthenticated, user} = useAuth();
+    const [dosAndDonts, setDosAndDonts ] = useState([]);
+
+    if (!isAuthenticated || (user && user.role !== 'patient')) {
+        return null; // or you can return a message or redirect to another page
+    }
+
+    useEffect(() => {
+        const fetchDosAndDonts = async () => {
+            try {
+                const response = await fetch(`/dosAndDonts/fears/${fearId}`);
+                const data = await response.json();
+                if (response.ok) {
+                    setDosAndDonts(data.dosAndDonts || []);
+                } else {
+                    console.error('Error fetching dos and donts:', data.message);
+                }
+            } catch (error) {
+                console.error('Error fetching dos and donts:', error);
+            }
+        };
+        fetchDosAndDonts();
+    }, [fearId]);
+
+    return (
+        <div>
+            <h1>Dos and Donts</h1>
+            <div className="dos-column">
+                <h3>Dos</h3>
+                <ul>
+                    {dosAndDonts
+                        .filter((item) => item.type === 'Do')
+                        .map((item) => (
+                            <li key={item._id}>{item.text}</li>
+                        ))}
+                </ul>
+            </div>
+            <div className="donts-column">
+                <h3>Donts</h3>
+                <ul>
+                    {dosAndDonts
+                        .filter((item) => item.type === "Don't")
+                        .map((item) => (
+                            <li key={item._id}>{item.text}</li>
+                        ))}
+                </ul>
+            </div>
+        </div>
+
+    );
+};
+export default DosAndDontsPatientPage;
+
