a11fa90e068756c4af594ab926506804a99c38db
PiaDue <pia.duenow@web.de>
2024-01-29 18:43:34 +0100

    Button UI

--- Frontend/hypno-buddy/src/components/RoadmapGoalTextbox.tsx
+++ Frontend/hypno-buddy/src/components/RoadmapGoalTextbox.tsx
@@ -102,21 +102,33 @@ function RoadmapGoalTextbox({ goal, handleComment }) {
                             goal.comments.length > 0 &&
                             goal.comments.map((c) =>
                                     !c.isPrivate ? (
-                                        <div className={`${styles.comment}`} key={c.commentID}>
-                                            <div>
-                                                  <span className={`${styles.writer}`}>
-                                                    {c.userID === user._id ? 'Du: ' :
-                                                        user.role === 'patient' ? 'Dein Therapeut: ' : `${selectedPatient?.name.first} ${selectedPatient?.name.last}:`
-                                                    }
-                                                  </span>
-                                                <span className={`${styles.date}`}>{getDate(c.creationDate)}</span>
-                                            </div>
-                                            {c.isPrivate ? (
-                                                <div className={`${styles.date}`}>
-                                                    <i>privater Kommentar</i>
+                                        <div className={"d-flex flex-row"}>
+                                            <div className={`${styles.comment}`} key={c.commentID}>
+                                                <div>
+                                                      <span className={`${styles.writer}`}>
+                                                        {c.userID === user._id ? 'Du: ' :
+                                                            user.role === 'patient' ? 'Dein Therapeut: ' : `${selectedPatient?.name.first} ${selectedPatient?.name.last}:`
+                                                        }
+                                                      </span>
+                                                    <span className={`${styles.date}`}>{getDate(c.creationDate)}</span>
                                                 </div>
-                                            ) : null}
-                                            <p className={`${styles.commentText}`}>{c.comment}</p>
+                                                {c.isPrivate ? (
+                                                    <div className={`${styles.date}`}>
+                                                        <i>privater Kommentar</i>
+                                                    </div>
+                                                ) : null}
+                                                <p className={`${styles.commentText}`}>{c.comment}</p>
+                                            </div>
+                                            {c.userID === user._id && (
+                                                <button className={`btn align-self-center ${allowEdit()} ${styles.statusBtn} ${styles.deleteBtn}${isActiveStatus("Geplant")}`}
+                                                        title="Löschen"
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                                    </svg>
+                                                </button>
+                                            )}
                                         </div>
                                     ) : null
                             )}
