8b41efac655decce64f721dcd9ef2a6fcf7f089d
Pia Dünow <89213910+PiaDue@users.noreply.github.com>
2024-01-30 23:44:54 +0100

    Delete comments (#173)
    * add docker file
    
    * configure db and frontend deployment
    
    * change frontend fetch statements to read url variable
    
    * backend service deployment to GCP
    
    * debug failing auth using cookies locally
    
    * hosting works except for cookies
    
    * switch to vercel frontend hosting
    
    * deployment working
    
    * switch frontend back to firebase
    
    * add terraform actions
    
    * add terraform actions
    
    * test actions
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * remove docu
    
    * comments for pr test (#161)
    
    * comments for pr test
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * add cloud to terraform actions
    
    * adjust actions
    
    * adjust main.tf
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * Deployment pr test (#165)
    
    * comments for pr test
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * add cloud to terraform actions
    
    * adjust actions
    
    * adjust main.tf
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * merge with main, change urls
    
    * dev changes
    
    * action change
    
    * fix typescript errors -> docker build error
    
    * add fonts and change db url
    
    * Button UI
    
    * add production and dev environment
    
    * fix url export
    
    * change README.md
    
    * fix register bug and startup requests
    
    * bugfix: private comment
    
    * deleteComment logic
    
    * vite.confic remove changes
    
    * fix patient relinking and dashboard race condition
    
    * fixed bugs
    
    - added key prop to comments
    - added frontend handling for empty comment submit
    
    * fix config and fillRule console error
    
    * fix vite version issue
    
    * fix scss
    
    * adjust url for delete comment route
    
    * fix issue
    
    ---------
    
    Co-authored-by: Justin <96232755+justingebert@users.noreply.github.com>

--- Frontend/hypno-buddy/src/components/RoadmapGoalTextbox.tsx
+++ Frontend/hypno-buddy/src/components/RoadmapGoalTextbox.tsx
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import styles from "../styles/RoadmapPage.module.scss";
 import GoalCommentForm from "./GoalCommentForm.tsx";
 import {useAuth} from "../contexts/AuthContext.tsx";
 import {useGoals} from "../contexts/GoalContext.tsx";
+import {FlashContext} from "../contexts/FlashContext.tsx";
 
 function RoadmapGoalTextbox({ goal, handleComment }) {
     const {user,selectedPatient} = useAuth();
-    const { updateGoal } = useGoals();
+    const { updateGoal, deleteComment } = useGoals();
     const [showDetails, setShowDetails] = useState(false);
 
     const getDate = (dueDate) => {
@@ -63,7 +64,7 @@ function RoadmapGoalTextbox({ goal, handleComment }) {
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-arrow-repeat text-primary`} viewBox="0 0 16 16">
                             <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
-                            <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+                            <path fillRule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
                         </svg>
                     </button>
                     <button className={`btn ${allowEdit()} ${styles.statusBtn} ${isActiveStatus("Erreicht")}`}
@@ -82,12 +83,12 @@ function RoadmapGoalTextbox({ goal, handleComment }) {
             <button className={`btn ${styles.detailsButton}`} onClick={() => setShowDetails(!showDetails)}>
                 {showDetails ? (
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-up" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+                        <path fillRule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
                     </svg>
                 ) : (
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
+                        <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
                     </svg>
                 )}
             </button>
@@ -101,22 +102,34 @@ function RoadmapGoalTextbox({ goal, handleComment }) {
                         {goal.comments &&
                             goal.comments.length > 0 &&
                             goal.comments.map((c) =>
-                                    !c.isPrivate ? (
-                                        <div className={`${styles.comment}`} key={c.commentID}>
-                                            <div>
-                                                  <span className={`${styles.writer}`}>
-                                                    {c.userID === user._id ? 'Du: ' :
-                                                        user.role === 'patient' ? 'Dein Therapeut: ' : `${selectedPatient?.name.first} ${selectedPatient?.name.last}:`
-                                                    }
-                                                  </span>
-                                                <span className={`${styles.date}`}>{getDate(c.creationDate)}</span>
-                                            </div>
-                                            {c.isPrivate ? (
-                                                <div className={`${styles.date}`}>
-                                                    <i>privater Kommentar</i>
+                                    !c.isPrivate || (c.userID === user._id) ? (
+                                        <div key={c._id} className={"d-flex flex-row commentBox"}>
+                                            <div className={`${styles.comment}`} key={c.commentID}>
+                                                <div>
+                                                      <span className={`${styles.writer}`}>
+                                                        {c.userID === user._id ? 'Du: ' :
+                                                            user.role === 'patient' ? 'Dein Therapeut: ' : `${selectedPatient?.name.first} ${selectedPatient?.name.last}:`
+                                                        }
+                                                      </span>
+                                                    <span className={`${styles.date}`}>{getDate(c.creationDate)}</span>
                                                 </div>
-                                            ) : null}
-                                            <p className={`${styles.commentText}`}>{c.comment}</p>
+                                                {c.isPrivate ? (
+                                                    <div className={`${styles.date}`}>
+                                                        <i>privater Kommentar</i>
+                                                    </div>
+                                                ) : null}
+                                                <p className={`${styles.commentText}`}>{c.comment}</p>
+                                            </div>
+                                            {c.userID === user._id && (
+                                                <button className={`btn btn-light align-self-center ${styles.statusBtn} ${styles.deleteBtn}`}
+                                                        title="Löschen" onClick={() => deleteComment(c)}
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                                    </svg>
+                                                </button>
+                                            )}
                                         </div>
                                     ) : null
                             )}
