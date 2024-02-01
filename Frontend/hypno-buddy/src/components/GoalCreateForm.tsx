3b1a8df54d57b631c7b98cef3f4ee59441120973
Justin <96232755+justingebert@users.noreply.github.com>
2024-02-01 13:49:12 +0100

    further styling, GoalCreateForm.

--- Frontend/hypno-buddy/src/components/GoalCreateForm.tsx
+++ Frontend/hypno-buddy/src/components/GoalCreateForm.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { RoadmapGoal } from "../types/Roadmap-Goal.ts";
-import styles from '../styles/GoalForm.module.scss';
+import styles from '../styles/Roadmap/GoalForm.module.scss';
 import {useGoals} from "../contexts/GoalContext.tsx";
+import style from '../styles/Roadmap/buttons.module.scss';
 
 interface GoalCreateFormProps {
     goalData: RoadmapGoal | null;
@@ -79,18 +80,18 @@ const GoalCreateForm: React.FC<GoalCreateFormProps> = ({ goalData, onSave, onClo
     const isEditing = goalData !== null && !goalData.isSubGoal;
 
     return (
-        <div className="modal show" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
-            <div className={styles.modalOverlay}>
-                <div className={`modal-dialog ${styles.modal}`} role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">{isEditing ? 'Ziel Bearbeiten' : 'Neues Ziel'}</h5>
-                            <button type="button" className="close bg-secondary" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
-                                <span aria-hidden="true">&times;</span>
+        <div className={`modal show `} tabIndex={-1} role="dialog" style={{ display: 'block' }}>
+            <div className={`${styles.modalOverlay}`}>
+                <div className={`modal-dialog ${styles.modalCustom}`} role="document">
+                    <div className={`modal-content  ${styles.modalCustom}`}>
+                        <div className={`modal-header ${styles.modalTitleCustom}`}>
+                            <h5 className={`modal-title`}>{isEditing ? 'Ziel Bearbeiten' : 'Neues Ziel'}</h5>
+                            <button type="button" className={`close ${style.btnDeleteCustom}`} data-dismiss="modal" aria-label="Close" onClick={handleClose}>
+                                <span aria-hidden="true" >&times;</span>
                             </button>
                         </div>
                         <form onSubmit={handleSubmit}>
-                            <div className="modal-body">
+                            <div className={`modal-body`}>
                                 {/* Title Field */}
                                 <div className="form-group">
                                     <label>Titel</label>
@@ -140,8 +141,8 @@ const GoalCreateForm: React.FC<GoalCreateFormProps> = ({ goalData, onSave, onClo
                                 )}
                             </div>
                             <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" onClick={handleClose}>Zurück</button>
-                                <button type="submit" className="btn btn-primary">{isEditing ? 'Speichern' : 'Hinzufügen'}</button>
+                                <button type="button" className={`btn btn-secondary ${style.btnDeleteCustom}`} onClick={handleClose}>Zurück</button>
+                                <button type="submit" className={`btn btn-primary ${style.btnPrimaryCustom}` }>{isEditing ? 'Speichern' : 'Hinzufügen'}</button>
                             </div>
                         </form>
                     </div>
