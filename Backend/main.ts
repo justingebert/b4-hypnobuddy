f2a55febadc0df0051cf58b6debea5248dd9eae9
Justin <96232755+justingebert@users.noreply.github.com>
2024-02-01 19:57:06 +0100

    Deployment - fix docker Build (#214)
    * add docker file
    
    * configure db and frontend deployment
    
    * change frontend fetch statements to read url variable
    
    * backend service deployment to GCP
    
    * debug failing auth using cookies locally
    
    * hosting works except for cookies
    
    * switch to vercel frontend hosting
    
    * deployment working
    
    * switch frontend back to firebase
    
    * add terraform actions
    
    * add terraform actions
    
    * test actions
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * remove docu
    
    * comments for pr test (#161)
    
    * comments for pr test
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * add cloud to terraform actions
    
    * adjust actions
    
    * adjust main.tf
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * Deployment pr test (#165)
    
    * comments for pr test
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * action adjustments
    
    * add cloud to terraform actions
    
    * adjust actions
    
    * adjust main.tf
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * adjsut actions
    
    * merge with main, change urls
    
    * dev changes
    
    * action change
    
    * fix typescript errors -> docker build error
    
    * add fonts and change db url
    
    * add production and dev environment
    
    * fix url export
    
    * change README.md
    
    * fix register bug and startup requests
    
    * fix patient relinking and dashboard race condition
    
    * fix vite version issue
    
    * fix scss
    
    * change deploy to lastest
    
    * change deploy to lastest correct iamge
    
    * Deployment add docu (#197)
    
    * add docu
    
    * add actions to deployment branch
    
    * add to docu
    
    * change backend Variable
    
    * adjust env location in actions
    
    * change env access
    
    * change env access
    
    * fix Frontend env
    
    * add docu
    
    * add actions to deployment branch
    
    * add to docu
    
    * change backend Variable
    
    * adjust env location in actions
    
    * change env access
    
    * change env access
    
    * test
    
    * change env to vite prefix
    
    * remove devops docu
    
    * correct, typo
    
    * fix backend production env (#203)
    
    * fix backend production env
    
    * fix backend production env
    
    * Deployment env fix (#204)
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * Deployment env fix (#205)
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * Deployment env fix (#206)
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * Deployment env fix (#207)
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * fix backend production env
    
    * test removing dockerd dev files
    
    * adjust docker action (#208)
    
    * showcase changes (#212)
    
    * prod env (#213)
    
    * Deployment final fixes (#215)
    
    * prod env
    
    * remove deployment branch from actions
    
    * add ci in again

--- Backend/main.ts
+++ Backend/main.ts
@@ -36,6 +36,7 @@ app.use(session({
         proxy: process.env.NODE_ENV === 'production',
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
     }
 }));
